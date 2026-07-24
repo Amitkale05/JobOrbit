d44f29f58fd237c3d0601bbf56d69647ab07423a
Amitkale05 <121034001+Amitkale05@users.noreply.github.com>
2026-07-24 11:34:44 +0530

    Delete src directory

--- src/utils/generateOtp.js
+++
@@ -1,7 +0,0 @@
-/**
- * Generates a random 6-digit numeric OTP
- * @returns {string} 6-digit OTP
- */
-export const generateOtp = () => {
-  return Math.floor(100000 + Math.random() * 900000).toString();
-};
